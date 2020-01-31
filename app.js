503ab5ab7583309c04757f44dc2cdf98874aeb57
Zavael <cccp14@atlas.sk>
2020-01-31 15:28:51 +0100

    redesigning server code structure

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
-var server = require('./src/server').createServer(),
-    configuration = require('./bin/configuration');
+const server = require('./src/server').createServer();
+const port = process.env.PORT || 3000;
+const log = require('./src/logger')(__filename.slice(__dirname.length + 1));
 
-configuration.applyConfiguration(server);
-
-server.listen(configuration.port, configuration.ipaddress, function () {
-    console.log('Node server started on %s:%d ...', configuration.ipaddress, configuration.port);
-    console.log('Accepting incoming requests: ', server.settings.env);  
-    console.log("Accepting incoming requests: %s", server.settings.env);  
-    console.info("sdfsdfdsfsfs");  
-    console.warn("warn");  
-    console.error("ssss");  
-});
+server.listen(port, () => log.info(`Listening on port ${port}`));
--- src/server.js
+++ src/server.js
@@ -1,13 +1,32 @@
-var express = require('express');
-var routes = require('./routes');
+const express = require('express');
+const routes = require('./routes');
+const log = require('./logger')(__filename.slice(__dirname.length + 1));
 
-exports.createServer = function createServer () {
+module.exports.createServer = createServer;
+
+function createServer() {
     
     var server = express();
+
+    log.debug('Setting up basic startup configuration...');
+    // config.applyConfiguration(server);
+    server.use(express.json());
+    server.use(express.urlencoded({ extended: true }));
     
     // attach router handlers
     routes.attachHandlers(server);
+
+    // setup general error handler
+    server.use(function (err, req, res, next) {
+        if (!err.logged) {
+            log.error('Express default error handler:', err);
+        }
+        res.status(500).send('Something broke!')
+    });
+    // setup 404 error handler
+    server.use(function (req, res, next) {
+        res.status(404).send({ error: 'Not found' });
+    });
     
-    return server;
-    
+    return server;    
 };
