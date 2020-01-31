ab957cb6dd22bca89537b81e68c72eab675bfb3c
Zavael <cccp14@atlas.sk>
2020-01-31 14:51:31 +0100

    custom logger

---
+++ src/logger.js
@@ -0,0 +1,40 @@
+const levels = ['DEBUG', 'INFO', 'WARN', 'ERROR'];
+
+class logger{
+
+    constructor(name){
+        this.name = name;
+    }
+    log(message, level, ...params) {
+        if (levels.indexOf(level) >= levels.indexOf(logger.level)) {
+            if (typeof message !== 'string') {
+                message = JSON.stringify(message);
+            };
+            
+            console.log(new Date() + ` [${level.padStart(5)}] ${this.name} ${message}`, ...params);
+        }
+    }
+    debug(message, ...params) {
+        this.log(message, 'DEBUG', ...params);
+    }
+    info(message, ...params) {
+        this.log(message, 'INFO', ...params);
+    }
+    warn(message, ...params) {
+        this.log(message, 'WARN', ...params);
+    }
+    error(message, ...params) {
+        this.log(message, 'ERROR', ...params);
+    }
+}
+
+if (process.env.NODE_ENV == "development") {
+    logger.level = 'DEBUG';    
+} else {
+    logger.level = 'INFO';
+}
+
+module.exports = (loggerName) => {
+    return new logger(loggerName);
+}
+
