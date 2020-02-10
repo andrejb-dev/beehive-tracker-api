9ff323da59278fc2997eb9cc74d73c2444d59354
Zavael <cccp14@atlas.sk>
2020-02-10 10:12:08 +0100

    fix: using extend of pg-promise to add new functions

--- src/db/index.js
+++ src/db/index.js
@@ -1,5 +1,36 @@
-const pgp = require('pg-promise')();
+const pgPromise = require('pg-promise');
 const log = require('../logger')(__filename.slice(__dirname.length + 1));
+
+const initOptions = {
+    extend(obj, dc) {
+        // Database Context (dc) is mainly useful when extending multiple databases with different access API-s.
+
+        // Do not use 'require()' here, because this event occurs for every task and transaction being executed,
+        // which should be as fast as possible.
+
+        // extension methods
+        obj.findById = (table, id) => {
+            log.debug('read ', table, id);
+            return db.one('Select * from ' + table + ' where id = $1', id);
+        };
+
+        obj.executeScript = (file, callback) => {
+            const queries = new pgp.QueryFile(file);
+            db.any(queries)
+                .then(res => {
+                    log.debug(`Executed script file ${file}: `, res);
+                    if (callback) {
+                        callback();
+                    }
+                })
+                .catch(err => {
+                    log.error(`Error executing file ${file}`, err);
+                })
+        };
+    }
+}
+
+const pgp = pgPromise(initOptions);
 
 // Database connection details;
 const cn = {
@@ -9,9 +40,8 @@ const cn = {
     user: 'postgres',
     password: 'postgres'
 };
-
 // Create db connection and verify it
-var db = pgp(process.env.DATABASE_URL || cn);
+const db = pgp(process.env.DATABASE_URL || cn);
 db.one('Select version()')
     .then(data => {
         log.info('Connected: ', data);
@@ -20,33 +50,4 @@ db.one('Select version()')
         log.error("Error connecting to db", error);
     });
 
-// 
-db.findById = (table, id) => {
-    log.debug('read ', table, id);
-    return db.one('Select * from ' + table + ' where id = $1', id);
-};
-
-// extension methods
-db.executeScript = (file, callback) => {
-    const queries = new pgp.QueryFile(file);
-    db.any(queries)
-        .then(res => {
-            log.debug(`Executed script file ${file}: `, res);
-            if (callback) {
-                callback();
-            }
-        })
-        .catch(err => {
-            log.error(`Error executing file ${file}`, err);
-        })
-};
-
 module.exports = db;
-
-// {
-//     hives: require('./db.hive')(db, log),
-//     yards: require('./db.yard')(db, log),
-//     inspections: require('./db.inspection')(db, log),
-//     todos: require('./db.todo')(db, log),
-//     users: require('./db.user')(db, log)
-// }
